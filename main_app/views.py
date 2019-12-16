54d72b61be509368239c0d259ad9f96a35d171fc
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-12-16 13:14:47 -0600

    define about URL and view function

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def home(request):
   return HttpResponse('<h1>Home View</h1>')
+
+def about(request):
+  return HttpResponse('<h1>About the DinoCollector</h1>')
