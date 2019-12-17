fdbd3da6c65c712a58af529134c9890c1421aad0
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-12-16 22:16:14 -0600

    simulate Dino model / create some dinos in dinos list

--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
+
+class Dino:
+    def __init__(self, name, nickname, description, era):
+        self.name = name
+        self.nickname = nickname
+        self.description = description
+        self.era = era
+
+
+dinos = [
+    Dino('Stegosaurus', 'Stego',
+         'This slow-moving plant-eater used spikes on its tail to fend off would-be predators.', 'Late Jurassic Period'),
+    Dino('Triceratops', 'Trike',
+         'This plant-eater used its horns for defending itself from predators.', 'Late Cretaceous Period'),
+    Dino('Tyrannosaurus', 'Carl', 'This infamous meat-eating predator, walked on two legs, balancing its huge head with a long and heavy tail that sometimes contained over 40 vertebrae.', 'Late Cretaceous Period')
+]
+
+
 def home(request):
-  return HttpResponse('<h1>Home View</h1>')
+    return HttpResponse('<h1>Home View</h1>')
+
 
 def about(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
+
 
 def dinos_index(request):
-  return render(request, 'dinos/index.html', { 
-      'dinos': dinos 
-    })  
+    return render(request, 'dinos/index.html', {
+        'dinos': dinos
+    })
