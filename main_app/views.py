eb1d6de129a78ce80120f14df9791347c61e0fb4
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-12-31 08:29:50 -0600

    delete fake data from the view

--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-class Dino:
-    def __init__(self, name, nickname, description, era):
-        self.name = name
-        self.nickname = nickname
-        self.description = description
-        self.era = era
-
-
-dinos = [
-    Dino('Stegosaurus', 'Stego',
-         'This slow-moving plant-eater used spikes on its tail to fend off would-be predators.', 'Late Jurassic Period'),
-    Dino('Triceratops', 'Trike',
-         'This plant-eater used its horns for defending itself from predators.', 'Late Cretaceous Period'),
-    Dino('Tyrannosaurus', 'Carl', 'This infamous meat-eating predator, walked on two legs, balancing its huge head with a long and heavy tail that sometimes contained over 40 vertebrae.', 'Late Cretaceous Period')
-]
-
+from .models import Dino
 
 def home(request):
     return render(request,'home.html')
@@ -28,6 +10,5 @@ def about(request):
 
 
 def dinos_index(request):
-    return render(request, 'dinos/index.html', {
-        'dinos': dinos
-    })
+    dinos = Dino.objects.all()
+    return render(request, 'dinos/index.html', { 'dinos': dinos })
