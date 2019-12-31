9d48f05bc83fd97e6574dffa3b3328d3a12127ed
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-12-31 07:32:45 -0600

    create Dino model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Dino(models.Model):
+    name = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    era = models.CharField(max_length=100)
