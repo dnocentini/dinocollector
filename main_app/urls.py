9204bf1cae5816eddf4e951948c74fac1fd203b1
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-12-31 08:59:08 -0600

    create Dino details page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('dinos/', views.dinos_index, name='index'),
+    path('dinos/<int:dino_id>/', views.dinos_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Dino
 
+
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
 
 
 def about(request):
@@ -11,4 +12,9 @@ def about(request):
 
 def dinos_index(request):
     dinos = Dino.objects.all()
-    return render(request, 'dinos/index.html', { 'dinos': dinos })
+    return render(request, 'dinos/index.html', {'dinos': dinos})
+
+
+def dinos_detail(request, dino_id):
+    dino = Dino.objects.get(id=dino_id)
+    return render(request, 'dinos/detail.html', {'dino': dino})
