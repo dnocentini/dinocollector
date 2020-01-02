855e7cc0dc9d04b942f1c6b10e78f6ae3eb62b59
Dinny Nocentini <dinny.nocentini@gmail.com>
2020-01-01 19:49:00 -0600

    update detail template for Rocks and create templates to rock list, form, detail and confirm delete

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Dino, Feeding
+from .models import Dino, Feeding, Rock
 
 # Register your models here.
 admin.site.register(Dino)
 admin.site.register(Feeding)
+admin.site.register(Rock)
